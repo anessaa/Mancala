cd0f26e4fb3774d6494182bb43a09cf245c2ff53
Anessa Arnold <anessaarnold@icloud.com>
2017-10-07 14:20:16 -0700

    some logic created

--- js/main.js
+++ js/main.js
@@ -1,11 +1,15 @@
+$(document).ready(function() {
 /*----- constants -----*/
 
 /*----- app's state (variables) -----*/
 var board
 // players objects
+var winner;
+var state;
 var player1
 var player2
-var score
+var idx
+var holeClicked
 var playerOneTurn = true
 /*----- cached element references -----*/
 var holes
@@ -22,12 +26,39 @@ $(".instructions").on('click', function() {
 
 })
 $(".gameBoard").on('click', 'div.hole', function() {
-    console.log('game board clicked')
-    // $('.holes').data('#id')
-    var idx = this.id
-    console.log(idx)
+    idx = this.id
+    console.log("hole " + idx + " on board")
+    spreadStones()
+    
 })
 /*----- functions -----*/
+function spreadStones() {
+    console.log("spread stones function called")  
+    var numStones = board[idx]
+    board[idx] = 0
+    // console.log(idx)
+    for (var offset = 1; offset <= numStones; offset++){
+        var nextHole = idx + offset
+        if (nextHole >= 14) nextHole = nextHole - 14 
+        board[nextHole]++   
+    } 
+    // var $holeValue = $('.hole')
+    //console.log($holeValue[1])
+    // var $holes = $('.hole')
+    // $(this).on('click', function(){
+    //     $holes.html()
+    // })
+    // if (board[1] === 0, board[2] === 0, board[3] === 0, board[4] === 0, board[5] === 0, board[6] === 0) {
+    //     console.log("checking if statement")
+    //     checkWin()
+    // }
+    render() 
+    console.log(numStones + " number of stones")
+    console.log(board)
+    
+    return numStones
+    
+}
 
 function freeTurn() {
 
@@ -37,11 +68,17 @@ function capture() {
 }
 
 function checkWin() {
-
+    console.log('running check win function')
+    if (player1 > player2) {
+        alert("Player 1 wins!")
+    } else if (player2 > player1) {
+        alert("Player 2 wins!")
+    }
+    
 }
 
 function render() {
-    
+
 }
 function init() {
     board = [
@@ -56,8 +93,9 @@ function init() {
         }, 
         4, 4, 4, 4, 4, 4,
     ]
-    $('.hole').text('4');
+    $('div.hole').html('4');
 }
 
 init()
 render()
+})

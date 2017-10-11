54a8f890f2ce103b366fd96f22c9459462cc2233
Anessa Arnold <anessaarnold@icloud.com>
2017-10-10 17:29:49 -0700

    spreading stones working and DOM updating

--- js/main.js
+++ js/main.js
@@ -2,96 +2,104 @@ $(document).ready(function() {
 /*----- constants -----*/
 
 /*----- app's state (variables) -----*/
-var board
+var board;
 // players objects
-var winner;
-var player1Score
-var player2Score
-var playerOneTurn = true
+var playerOneTurn;
+
+
 /*----- cached element references -----*/
-var holes
-// var $resetButton = $(".resetButton")
-// var $infoButton = $(".instructions")
+var $holes = $('.hole, .bucket1, .bucket2');
+console.log($holes.length)
+
 /*----- event listeners -----*/
 $(".resetButton").on('click', function() {
-    console.log('reset button clicked')
+    init();
+    render();
+});
 
-})
 $(".instructions").on('click', function() {
     console.log('instruction button clicked')
+});
 
-})
-$(".gameBoard").on('click', 'div.hole', function() {
+$("div.hole").on('click', function() {
     var idx = this.id
     idx = parseInt(idx)
-    console.log("hole " + idx + " on board")
     spreadStones(idx)
-    render() 
-    
+    playerOneTurn = !playerOneTurn;
+    winner = checkWin();
+    render();
 })
+
 /*----- functions -----*/
 function spreadStones(idx) {
-    console.log("spread stones function called")
-    //console.log(idx)  
-    var numStones = board[idx]
-    board[idx] = 0
-    // console.log(idx)
-    for (var offset = 1; offset <= numStones; offset++){
-        var nextHole = idx + offset
-        if (nextHole >= 14) nextHole = nextHole - 14 
-        board[nextHole]++   
-    } 
-    // var $holeValue = $('.hole')
-    //console.log($holeValue[1])
-    // var $holes = $('.hole')
-    // $(this).on('click', function(){
-    //     $holes.html()
-    // })
-    // if (board[1] === 0, board[2] === 0, board[3] === 0, board[4] === 0, board[5] === 0, board[6] === 0) {
-    //     console.log("checking if statement")
-    //     checkWin()
-    // }
-    //console.log(numStones + " number of stones")
-    console.log(board)
+    idx = parseInt(idx);
+    var numStones = board[idx];
+    board[idx] = 0;
+    var bucketOffSet = 0;
+    console.log(idx)
+    for (var offset = 1; offset <= numStones; offset++) { 
+        var nextHole = idx + offset;
+        if (nextHole >= 14) nextHole = nextHole - 14;
+        if((playerOneTurn && nextHole === 0) || (!playerOneTurn && nextHole === 7)) bucketOffSet++;
+
+        board[nextHole + bucketOffSet]++;
+
+    }
     return numStones;
 }
 
-function freeTurn() {
-
-}
-function capture() {
-
-}
-
 function checkWin() {
-    console.log('running check win function')
-    if (player1 > player2) {
-        alert("Player 1 wins!")
-    } else if (player2 > player1) {
-        alert("Player 2 wins!")
+    var winner;
+    var player1Score = board[7];
+    var player2Score = board[0];
+    if (board[1] === 0 && board[2] === 0 && board[3] === 0 && board[4] === 0 && board[5] === 0 && board[6] === 0
+    || board[8] === 0 && board[9] === 0 && board[10] === 0 && board[11] === 0 && board[12] === 0 && board[13] === 0){
+        if (player1Score > player2Score) {
+            return "Player 1 wins!"
+        } else if (player2Score > player1Score) {
+            return "Player 2 wins!"
+        } else {
+            return "Tie!"
+        }
     }
-    
 }
 
 function render() {
+    if (winner) {
+        $('.status').html(`Winner is ${winner}`)
+    } else {
+        $('.status').html(`Next player is ${(playerOneTurn ? 'Player 1' : 'Player 2')}`)
+        }
+        
+    if (playerOneTurn) {
+        $('.topSeperator').addClass('active')
+        $('.bottomSeperator').removeClass('active')
+    } else if (!playerOneTurn) {
+        $('.topSeperator').removeClass('active')
+        $('.bottomSeperator').addClass('active')
+    }
+    $(".player1").html(`Player 1: ${board[7]}`)
+    $(".player2").html(`Player 2: ${board[0]}`)
+    $holes.each(function() {
+        var idx = this.id;
+        $(this).html(board[idx]);
+        });
+    }
 
-}
 function init() {
     board = [
-        {
-            bucketAmt: 0, 
-            playerOneOrTwo: 1,
-        }, 
-        4, 4, 4, 4, 4, 4, 
-        {
-            bucketAmt: 0, 
-            playerOneOrTwo: 2,
-        }, 
-        4, 4, 4, 4, 4, 4,
-    ]
-    $('div.hole').html('4');
+        0, 
+        4, 4, 4, 4, 4, 10, 
+        0, 
+        4, 4, 4, 4, 4, 10
+    ];
+    playerOneTurn = true;
+    winner = null;
+    $('.status').html("Player one starts!")
 }
 
-init()
-render()
-})
+init();
+render();
+
+});
+

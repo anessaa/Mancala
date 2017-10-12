b112fe2898c26fc812f684a813ba1764169aea2a
Anessa Arnold <anessaarnold@icloud.com>
2017-10-12 09:57:47 -0700

    pebble images

--- js/main.js
+++ js/main.js
@@ -1,27 +1,62 @@
 $(document).ready(function() {
 /*----- constants -----*/
-// var stoneImgs = [
-//     '',
-//     "img/pebble1.png",
-//     null,
-//     "img/pebble1",
-//     "https://i.pinimg.com/originals/4d/1f/c7/4d1fc7896d3da8fe409bbc480bce3a5c.jpg"
-// ];
+var stoneImgs = [
+    '',
+    "img/pebble1.png",
+    "img/pebble2.png",
+    "img/pebble3.png",
+    "img/pebble4.png",
+    "img/pebble5.png",
+    "img/pebble6.png",
+    "img/pebble7.png",
+    "img/pebble8.png",
+    "img/pebble9.png",
+    "img/pebble10.png",
+    "img/pebble11.png",
+    "img/pebble12.png",
+    "img/pebble13.png",
+    "img/pebble14.png",
+    "img/pebble15.png",
+    "img/pebble16.png",
+    "img/pebble17.png",
+    "img/pebble18.png",
+    "img/pebble19.png",
+    "img/pebble20.png",
+    "img/pebble21.png", 
+    "img/pebble22.png", 
+    "img/pebble23.png", 
+    "img/pebble24.png",    
+    "img/pebble25.png", 
+    "img/pebble26.png", 
+    "img/pebble27.png", 
+    "img/pebble28.png", 
+    "img/pebble29.png", 
+    "img/pebble30.png", 
+    "img/pebble31.png", 
+    "img/pebble32.png", 
+    "img/pebble33.png", 
+    "img/pebble34.png", 
+    "img/pebble35.png" 
+];
 
 /*----- app's state (variables) -----*/
 var board;
 var playerOneTurn;
 var freeTurn;
+var resetButtonClicked; 
 /*----- cached element references -----*/
 var $holes = $('.hole, .bucket1, .bucket2');
 /*----- event listeners -----*/
-$(".resetButton").on('click', function() {
+resetButtonClicked = false;
+$(".resetButton").on('click', function() {   
+    resetButtonClicked = true; 
     init();
     render();
 });
 
+$(".hidden").hide();
 $(".instructions").on('click', function() {
-   console.log("instructions button clicked")
+    $(".info, .hidden").toggle()   
 });
 
 $("div.hole").on('click', function() {
@@ -54,7 +89,6 @@ function spreadStones(idx) {
     var minHoleIdx = playerOneTurn ? 1 : 8;
     var maxHoleIdx = playerOneTurn ? 6 : 13;
     if ((lastHoleIdx >= minHoleIdx && lastHoleIdx <= maxHoleIdx && board[lastHoleIdx] === 1) && board[oppIdx]) {
-        console.log('in if')
         var numStones = board[oppIdx] + board[lastHoleIdx];
         board[oppIdx] = board[lastHoleIdx] = 0;
         board[playerOneTurn ? 7 : 0] += numStones;
@@ -72,38 +106,40 @@ function checkWin() {
     if (board[1] === 0 && board[2] === 0 && board[3] === 0 && board[4] === 0 && board[5] === 0 && board[6] === 0
     || board[8] === 0 && board[9] === 0 && board[10] === 0 && board[11] === 0 && board[12] === 0 && board[13] === 0){
         if (player1Score > player2Score) {
-            return "Player 1 wins!"
+            return "Player 1 wins!";
         } else if (player2Score > player1Score) {
-            return "Player 2 wins!"
+            return "Player 2 wins!";
         } else {
-            return "Tie!"
+            return "Tie!";
         }
     }
 }
 
 function render() {
     if (winner) {
-        $('.status').html(`Winner is ${winner}`)
+        $('.status').html(`Winner is ${winner}`);
     } else if (freeTurn) {
-        $('.status').html(`Free Turn. Player ${playerOneTurn ? 'one' : 'two'} goes again.`);
+        freeTurn = false;
+        $('.status').html(`Free Turn. Player ${playerOneTurn ? 'one' : 'two'} goes again.`); 
     } else {
-        $('.status').html(`Next player is ${(playerOneTurn ? 'Player 1' : 'Player 2')}`)
+        $('.status').html(`${(playerOneTurn ? "Player 1's" : "Player 2's")} Turn`);
     }
-        
+
+
     if (playerOneTurn) {
         $('.topSeperator').addClass('active')
-        $('.bottomSeperator').removeClass('active')
+        $('.bottomSeperator').removeClass('active');
     } else if (!playerOneTurn) {
         $('.topSeperator').removeClass('active')
-        $('.bottomSeperator').addClass('active')
+        $('.bottomSeperator').addClass('active');
     }
     
-    $(".player1").html(`Player 1: ${board[7]}`)
-    $(".player2").html(`Player 2: ${board[0]}`)
+    $(".player1").html(`Player 1: ${board[7]}`);
+    $(".player2").html(`Player 2: ${board[0]}`);
     $holes.each(function() {
         var idx = this.id;
-        // $(this).html(`<img src="${stoneImgs[board[idx]]}" width="50">`);
-        $(this).html(board[idx]);
+        $(this).html(`<img src="${stoneImgs[board[idx]]}">`);
+        //$(this).html(board[idx]);
     });
 }
 
@@ -116,10 +152,10 @@ function init() {
     ];
     playerOneTurn = true;
     winner = null;
-    $('.status').html("Player one starts!")    
 }
 
 init();
 render();
 });
+
 
--- readme.md
+++ readme.md
@@ -6,38 +6,37 @@ Set the board:
 
 Rules:
 
-if you run into your own store, depost one piece in it. If you run into your opponent's store, skip it.
+If you run into your own bucket, deposit one piece in it. If you run into your opponent's store, skip it.
 
-Free turn - if the last piece you drop is in your own store.
+Free turn - If the last piece you drop is in your own store.
 
-Capture - if last piece you drop is in an empty hole on your side, you caputre that piece and any pieces in the hole directly opposit.
+Capture - If last piece you drop is in an empty hole on your side, you capture that piece and any pieces in the hole directly opposite.
 
-End - game ends when all six spaces on one side of the board are empty. the player who still has pieces on his side of the board when the game ends captures all of those pieces.
+End - Game ends when all six spaces on one side of the board are empty. the player who still has pieces on his side of the board when the game ends captures all of those pieces.
 
-Winner: 
-Count all the pieces in each store. The winner is the player with the most pieces.
-
+Winner - Count all the pieces in each store. The winner is the player with the most pieces.
 
 
 
 
-populate the board with 4 stones in each hole
 
-create player 1 and player 2
+Populate the board with 4 stones in each hole.
 
-player 1 picks up any batch of stones on their side and drop one stone in each hole in a counter-clockwise motion
+Create player 1 and player 2.
 
-keep track of each players score
+Player 1 picks up any batch of stones on their side and drop one stone in each hole in a counter-clockwise motion.
 
-if the player encounters their winning bucket, drop one stone in
+Keep track of each players score.
 
-if the player encounters their opponent's bucket, ignore and continue dropping stones
+If the player encounters their winning bucket, drop one stone in.
 
-the player can get a free turn if the last stone is dropped in their bucket
+If the player encounters their opponent's bucket, ignore and continue dropping stones.
 
-the player can capture stones if the last piece dropped in was in an empty hole. the player grabs the stones in the opponents hole.
+The player can get a free turn if the last stone is dropped in their bucket.
 
-the game ends when all six spaces on the board are cleared. the player who still has pieces on his side of the board when game ends gets all of those pieces.
+The player can capture stones if the last piece dropped in was in an empty hole. the player grabs the stones in the opponents hole.
+
+The game ends when all six spaces on the board are cleared. the player who still has pieces on his side of the board when game ends gets all of those pieces.
 
 variables:
 playerOne
